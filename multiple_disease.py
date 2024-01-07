421318ccd3980b5afb1627022baeb17025dda3f4
Akbar <114834354+AkbarBunyad@users.noreply.github.com>
2024-01-08 01:40:10 +0400

    Update multiple_disease.py

--- multiple_disease.py
+++ multiple_disease.py
@@ -8,7 +8,7 @@ Created on Sun Jan  7 23:02:34 2024
 import pickle
 import streamlit as st
 import sys
-sys.path.append(r'C:\Users\user\anaconda3\envs\MachineLearning\Lib\site-packages')
+sys.path.insert(1, r'C:\Users\user\anaconda3\envs\MachineLearning\Lib\site-packages')
 from streamlit_option_menu import option_menu
 
 
