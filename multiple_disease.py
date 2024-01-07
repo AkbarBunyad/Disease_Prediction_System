da0060134865f2ae21e961c587505b98c1e0b3d5
Akbar <114834354+AkbarBunyad@users.noreply.github.com>
2024-01-08 00:34:23 +0400

    Add files via upload

---
+++ multiple_disease.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  7 23:02:34 2024
+
+@author: user
+"""
+
+import pickle
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+
+#loading models
+
+diabetes_model = pickle.load(open('C:/Users/user/Downloads/diabetes_model.sav', 'rb'))
+
+heart_model = pickle.load(open('C:/Users/user/Downloads/heart_disease.sav','rb'))
+
+#siderbar for navigate
+with st.sidebar:
+    selected = option_menu('Multiple Disease Prediction System',
+                           ['Diabetes Prediction',
+                           'Heart Disease Prediction'],
+                           icons = ['activity', 'heart'],
+                           default_index = 0)
+
+if selected == 'Diabetes Prediction':
+    st.title('Diabetes Prediction using ML')
+    
+    Pregrancies = st.text_input('Number of Pregrancies')
+    Glucose = st.text_input('Glucose Level')
+    BloodPressure = st.text_input('Blood Pressure value')
+    SkinThickness = st.text_input('Skin Thickness value')
+    Insulin = st.text_input('Insulin level')
+    BMI = st.text_input('BMI value')
+    DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+    Age = st.text_input('Age of the Person')
+    
+    diab_diagnosis = ''
+    
+    if st.button('Diabetes Test Result'):
+        diab_prediction = diabetes_model.predict([[Pregrancies, Glucose,
+                                                   BloodPressure, SkinThickness,
+                                                   Insulin, BMI,
+                                                   DiabetesPedigreeFunction,
+                                                   Age]])
+        if diab_prediction[0] == 1:
+            diab_diagnosis = 'The person is Diabetic'
+        else:
+            diab_diagnosis = 'The person is not Diabetic'
+    st.success(diab_diagnosis)
+
+if selected == 'Heart Disease Prediction':
+    st.title('Heart Disease Prediction using ML')
+    col1, col2, col3 = st.columns(3)
+    
+    with col1:
+        age = st.text_input('Age')
+        
+    with col2:
+        sex = st.text_input('Sex')
+        
+    with col3:
+        cp = st.text_input('Chest Pain types')
+        
+    with col1:
+        trestbps = st.text_input('Resting Blood Pressure')
+        
+    with col2:
+        chol = st.text_input('Serum Cholestoral in mg/dl')
+        
+    with col3:
+        fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
+        
+    with col1:
+        restecg = st.text_input('Resting Electrocardiographic results')
+        
+    with col2:
+        thalach = st.text_input('Maximum Heart Rate achieved')
+        
+    with col3:
+        exang = st.text_input('Exercise Induced Angina')
+        
+    with col1:
+        oldpeak = st.text_input('ST depression induced by exercise')
+        
+    with col2:
+        slope = st.text_input('Slope of the peak exercise ST segment')
+        
+    with col3:
+        ca = st.text_input('Major vessels colored by flourosopy')
+        
+    with col1:
+        thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
+        
+        
+     
+     
+    # code for Prediction
+    heart_diagnosis = ''
+    
+    # creating a button for Prediction
+    
+    if st.button('Heart Disease Test Result'):
+        heart_prediction = heart_disease_model.predict([[age, sex, cp, trestbps, chol, fbs, restecg,thalach,exang,oldpeak,slope,ca,thal]])                          
+        
+        if (heart_prediction[0] == 1):
+          heart_diagnosis = 'The person is having heart disease'
+        else:
+          heart_diagnosis = 'The person does not have any heart disease'
+        
+    st.success(heart_diagnosis)
